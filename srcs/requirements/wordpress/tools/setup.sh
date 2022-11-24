3a6c0214b1dbf9dc063e302a3c6ef69085abbc66
Ajjig <majjig@pm.me>
2022-11-24 19:21:34 +0100

    Added setup.sh

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,10 +1,14 @@
-sed "s/\/run\/php\/php7.3-fpm.sock/9000" --in-place "/etc/php/7.3/fpm/pool.d/www.conf"
+sed "s/\/run\/php\/php7.3-fpm.sock/9000/" --in-place "/etc/php/7.3/fpm/pool.d/www.conf"
 
 ls /var/www/html/wp-config.php &> /dev/null
 
 if [ $? ] ; then
-	wp core download --path=/var/www/html --allow-root
-	wp config create --dbname=$MARIADB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --path=/var/www/html --allow-root --skip-check
-	wp core install --url=$DOMAIN_NAME --title=wordpress --admin_user=$DB_ADMINISTRATOR --admin_password=$DB_ADMINISTRATOR_PASSWORD --admin_email=$DB_ADMINISTRATOR_EMAIL --allow-root --path=/var/www/html
-	wp user create $WP_USER_NAME $WP_EMAIL --user_pass=$WP_USER_PASS --allow-root --path=/var/www/html
+	wp core download --path=/var/www/html --allow-root \
+	wp config create --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} \
+		--dbpass=${MARIADB_PASSWORD} --dbhost=mariadb \
+		--path=/var/www/html --allow-root --skip-check
+	wp core install --url=${DOMAIN_NAME} --title=wordpress --admin_user=${DB_ADMINISTRATOR} \
+		--admin_password=${DB_ADMINISTRATOR_PASSWORD} --admin_email=${DB_ADMINISTRATOR_EMAIL} \
+		--allow-root --path=/var/www/html
+	wp user create ${WP_USER_NAME} ${WP_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root --path=/var/www/html
 fi
