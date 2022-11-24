46d50f69bf1a6464e62860eebb6bf8855d70ec6f
Ajjig <majjig@pm.me>
2022-11-24 16:58:10 +0100

    :heavy_plus_sign: setup.sh

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,8 @@
+ls /var/www/html/wp-config.php
+
+if [ $? ] ; then
+	wp core download --path=/var/www/html --allow-root
+	wp config create --dbname=$MARIADB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --path=/var/www/html --allow-root --skip-check
+	wp core install --url=$DOMAIN_NAME --title=wordpress --admin_user=$DB_ADMINISTRATOR --admin_password=$DB_ADMINISTRATOR_PASSWORD --admin_email=$DB_ADMINISTRATOR_EMAIL --allow-root --path=/var/www/html
+	wp user create $WP_USER_NAME $WP_EMAIL --user_pass=$WP_USER_PASS --allow-root --path=/var/www/html
+fi
