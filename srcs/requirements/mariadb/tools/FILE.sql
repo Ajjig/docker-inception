4b4e2135ea140fde0851135397415deab7e4e75f
Ajjig <majjig@pm.me>
2022-11-24 12:11:40 +0100

    update aqlfile

--- srcs/requirements/mariadb/tools/FILE.sql
+++ srcs/requirements/mariadb/tools/FILE.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wpdb ;
-CREATE USER IF NOT EXISTS 'majjig'@'localhost' IDENTIFIED BY 'majjig' ;
+CREATE USER IF NOT EXISTS 'majjig'@'localhost' IDENTIFIED BY 'majjig@@A1' ;
 GRANT ALL PRIVILEGES ON wpdb.* TO 'majjig'@'localhost' ;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'the-su-majjig' ;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'the-su-majjig@@A1' ;
 FLUSH PRIVILEGES ;
