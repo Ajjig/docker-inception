a67f7b86305a0279e53afd6e00390be964ee3bbe
Ajjig <majjig@pm.me>
2022-11-25 13:47:31 +0100

    Revert ":hammer: fix mariadb issue"
    This reverts commit 3c5b0132f0652d5846b37478783edeb1039575f2.

--- srcs/requirements/mariadb/tools/FILE.sql
+++ srcs/requirements/mariadb/tools/FILE.sql
@@ -2,4 +2,4 @@ CREATE DATABASE IF NOT EXISTS wpdb ;
 CREATE USER IF NOT EXISTS 'majjig'@'localhost' IDENTIFIED BY 'majjig@@A1' ;
 GRANT ALL PRIVILEGES ON wpdb.* TO 'majjig'@'localhost' ;
 FLUSH PRIVILEGES ;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'the-su-majjig@@A1' ;
+ALTER USER 'su-majjig'@'localhost' IDENTIFIED BY 'the-su-majjig@@A1' ;
