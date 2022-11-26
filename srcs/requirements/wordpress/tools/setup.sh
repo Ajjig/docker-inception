7c1411483d755c248897b583139d1d091207c38c
Ajjig <majjig@pm.me>
2022-11-26 15:52:51 +0100

    :bug: fix setup.sh issues

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,22 +9,25 @@ touch /run/php/php7.3-fpm.pid;
 chown -R www-data:www-data ${WP_INSTALLATION_PATH}/* &> /dev/null
 
 if [ ! -f ${WP_INSTALLATION_PATH}/wp-config.php ] ; then
+
 	mkdir -p ${WP_INSTALLATION_PATH}
-	
+
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	
+
 	chmod +x wp-cli.phar
-	
+
 	mv wp-cli.phar /usr/local/bin/wp && chmod 755 /usr/local/bin/wp
 
 	wp core download --path=${WP_INSTALLATION_PATH} --allow-root
-	
+
 	wp config create --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} \
 		--dbpass=${MARIADB_PASSWORD} --dbhost=mariadb \
 		--path=${WP_INSTALLATION_PATH} --allow-root --skip-check
+
 	wp core install --url=${DOMAIN_NAME} --title=wordpress --admin_user=${DB_ADMINISTRATOR} \
 		--admin_password=${DB_ADMINISTRATOR_PASSWORD} --admin_email=${DB_ADMINISTRATOR_EMAIL} \
 		--allow-root --path=${WP_INSTALLATION_PATH}
+
 	wp user create ${WP_USER_NAME} ${WP_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root --path=${WP_INSTALLATION_PATH}
 fi
 
